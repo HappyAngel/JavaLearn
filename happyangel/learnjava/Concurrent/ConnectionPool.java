d5eda8a545197eeb531b5ade8c3ae5b5c33b3d83
HappyAngel <HappyAngel880503@gmail.com>
2016-09-21 22:29:10 +0800

    Add example of ConnectPool

---
+++ happyangel/learnjava/Concurrent/ConnectionPool.java
@@ -0,0 +1,104 @@
+package happyangel.learnjava.Concurrent;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.util.LinkedList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Lei on 2016/9/21.
+ */
+public class ConnectionPool {
+    private LinkedList<Connection> pool = new LinkedList<>();
+
+    public ConnectionPool(int initialSize) {
+        if (initialSize > 0) {
+            for (int i=0; i < initialSize; i++) {
+                pool.addLast(ConnectDriver.CreateConnection());
+            }
+        }
+    }
+
+    public void releaseConnection(Connection connection) {
+        if (connection != null) {
+            synchronized (pool) {
+                pool.addLast(connection);
+                pool.notifyAll();
+            }
+        }
+    }
+
+    public Connection fetchConnection(long mills) throws InterruptedException {
+        synchronized (pool) {
+            if (mills <= 0) {
+                while (pool.isEmpty()) {
+                    pool.wait();
+                }
+                return pool.removeFirst();
+            } else {
+                long future = System.currentTimeMillis() + mills;
+                long remaining = mills;
+                while (pool.isEmpty() && remaining > 0) {
+                    pool.wait(remaining);
+                    remaining = future - System.currentTimeMillis();
+                }
+                Connection result = null;
+                if (!pool.isEmpty()) {
+                    result = pool.removeFirst();
+                }
+                return result;
+            }
+        }
+    }
+}
+
+public class ConnectDriver {
+    static class ConnectionHandler implements InvocationHandler {
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().equals("commit")) {
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+            return null;
+        }
+    }
+
+    public static final Connection CreateConnection() {
+        return (Connection) Proxy.newProxyInstance(ConnectDriver.class.getClassLoader(), new Class<>[]{Connection.class}, new ConnectionHandler());
+    }
+}
+
+public class ConnectPoolTest {
+    static ConnectionPool pool = new ConnectionPool(10);
+
+    static CountDownLatch start = new CountDownLatch(1);
+
+    static CountDownLatch end;
+
+    public static void main(String[] args) throws Exception {
+        int threadCount = 10;
+        end = new CountDownLatch(threadCount);
+        int count = 20;
+        AtomicInteger got = new AtomicInteger();
+        AtomicInteger notGot = new AtomicInteger();
+
+        for (int i=0; i < threadCount; i++) {
+            Thread thread = new Thread(new ConnectionRunner(count, got, notGot), "ConnectionRunnerThread");
+            thread.start();
+        }
+        start.countDown();
+        end.await();
+        System.out.println("total invoke: " + (threadCount * count));
+        System.out.println("got connect: " + got);
+        System.out.println("not got connection: " + notGot);
+    }
+
+    static class ConnectRunner implements Runnable {
+
+    }
+}
+
