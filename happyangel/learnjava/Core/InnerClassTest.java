ea8c2f1efee5f9d61d9b6de6848a2c3157028af8
xionglei <xionglei@mi.com>
2016-10-11 10:35:21 +0800

    [Java Inner Class] add static inner class example and more comments

--- happyangel/learnjava/Core/InnerClassTest.java
+++ happyangel/learnjava/Core/InnerClassTest.java
@@ -12,8 +12,8 @@ import javax.swing.*;
  */
 public class InnerClassTest {
     public static void main(String[] args) {
-        TalkingClock clock = new TalkingClock(1000, true);
-        clock.start();
+        TalkingClock clock = new TalkingClock();
+        clock.start(1000, true);
 
         JOptionPane.showMessageDialog(null, "Quit program?");
         System.exit(0);
@@ -24,29 +24,52 @@ public class InnerClassTest {
     A clock that prints the time in regular intervals
  */
 class TalkingClock {
-    private int interval;
-    private boolean beep;
 
-    public TalkingClock(int interval, boolean beep) {
-        this.interval = interval;
-        this.beep = beep;
-    }
+    public void start(int interval, boolean beep) {
 
-    public void start() {
-        ActionListener listener = new TimePrinter();
+        /*
+            this is the form of anonymous inner class,
+            the syntax is like:
+
+            SuperType(Constructor parameters) {
+                inner class methods
+
+            }
+
+            inner class can only implements interfaces or extend classes
+
+            the reason to use inner class is it can access outer class's variables including private ones.
+            Also it can be hidden from other classes.
+         */
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Date now = new Date();
+                System.out.println("At the tone, the time is " + now);
+                // note there, the inner class TimePrinter CAN ACCESS the PRIVATE variable beep of the outer class here
+                // you can also use TalkingClock.this.beep instead of beep here
+                if (beep) {
+                    System.out.println("Beeping...");
+                }
+            }
+        };
         Timer t = new Timer(interval, listener);
         t.start();
     }
 
-    public class TimePrinter implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            Date now = new Date();
-            System.out.println("At the tone, the time is " + now);
-            // note there, the inner class TimePrinter CAN ACCESS the PRIVATE variable beep of the outer class here
-            // you can also use TalkingClock.this.beep instead of beep here
-            if (beep) {
-                System.out.println("Beeping...");
-            }
-        }
-    }
+    /*
+        this is the form of normal inner class
+     */
+
+//    public class TimePrinter implements ActionListener {
+//        public void actionPerformed(ActionEvent event) {
+//            Date now = new Date();
+//            System.out.println("At the tone, the time is " + now);
+//            // note there, the inner class TimePrinter CAN ACCESS the PRIVATE variable beep of the outer class here
+//            // you can also use TalkingClock.this.beep instead of beep here
+//            if (beep) {
+//                System.out.println("Beeping...");
+//            }
+//        }
+//    }
 }
---
+++ happyangel/learnjava/Core/StaticInnerClassTest.java
@@ -0,0 +1,61 @@
+package happyangel.learnjava.Core;
+
+/**
+ * Created by xionglei on 16-10-11.
+ *
+ * from Cay Horstmann's <Core Java>
+ *
+ */
+public class StaticInnerClassTest {
+    public static void main(String[] args) {
+        double[] d = new double[20];
+        for (int i=0; i < d.length; i++) {
+            d[i] = 100 * Math.random();
+        }
+
+        ArrayAlg.Pair p = ArrayAlg.minmax(d);
+
+        System.out.println("min=" + p.getFirst());
+        System.out.println("max=" + p.getSecond());
+    }
+}
+
+class ArrayAlg {
+
+    // here we don'e need Pair to hold the reference to ArrayAlg
+    // this is why we use static inner class instead of normal inner class
+    public static class Pair {
+        private double first;
+        private double second;
+
+        public Pair(double f, double s) {
+            first = f;
+            second = s;
+        }
+
+        public double getFirst() {
+            return first;
+        }
+
+        public double getSecond() {
+            return second;
+        }
+    }
+
+    public static Pair minmax(double[] values) {
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+
+        for (double v : values) {
+            if (min > v) {
+                min = v;
+            }
+
+            if (max < v) {
+                max = v;
+            }
+        }
+
+        return new Pair(min, max);
+    }
+}
