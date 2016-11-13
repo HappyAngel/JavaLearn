fdca66607d1e34bcca401968927d53abfe5c8a78
HappyAngel <HappyAngel880503@gmail.com>
2016-11-13 17:33:14 +0800

    [Java concurrent] add LinkedQueue example to demonstrate Condition usuage

---
+++ happyangel/learnjava/Concurrent/LinkedQueue.java
@@ -0,0 +1,64 @@
+package happyangel.learnjava.Concurrent;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Lei on 2016/11/13.
+ *
+ * examples from the art of multiprocessor programming
+ *
+ * to demonstrate Condition
+ */
+public class LinkedQueue<T> {
+    final Lock lock = new ReentrantLock();
+    final Condition notFull = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
+    final T[] items;
+    int tail, head, count;
+
+    public LinkedQueue(int capacity) {
+        items = (T[])new Object(100);
+    }
+
+    public void enq(T x) {
+        lock.lock();
+        try {
+            while (count == items.length) {
+                notFull.await();
+            }
+            items[tail] = x;
+            if (++tail == items.length) {
+                tail = 0;
+            }
+            ++count;
+            notEmpty.signal();
+        } catch (InterruptedException ex) {
+            // other thread interrupt
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T deq() {
+        lock.lock();
+        try {
+            // should keep check, even if get signalled
+            while (count == 0) {
+                notEmpty.await();
+            }
+            T x = items[head];
+            if (++head == items.length) {
+                head = 0;
+            }
+            --count;
+            notFull.signal();
+            return x;
+        } catch (InterruptedException ex) {
+
+        } finally {
+            lock.unlock();
+        }
+    }
+}
