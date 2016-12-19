06fe0ce60c3d8dbb168a4697ce8468fbf2ea4abc
xionglei <xionglei@mi.com>
2016-12-19 18:15:04 +0800

    add demo for Semaphores

---
+++ happyangel/learnjava/Concurrent/SemaphoreDemo.java
@@ -0,0 +1,45 @@
+package happyangel.learnjava.Concurrent;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Lei on 16-12-19.
+ *
+ * from Java Concurrent in Practice
+ */
+public class SemaphoreDemo {
+
+    class BoundedHashSet<T> {
+        private final Set<T> set;
+        private final Semaphore sem;
+
+        public BoundedHashSet(int bound) {
+            this.set = Collections.synchronizedSet(new HashSet<T>());
+            sem = new Semaphore(bound);
+        }
+
+        public boolean add(T o) throws InterruptedException {
+            sem.acquire();
+            boolean wasAdded = false;
+            try {
+                wasAdded = set.add(o);
+                return wasAdded;
+            } finally {
+                if (!wasAdded) {
+                    sem.release();
+                }
+            }
+        }
+
+        public boolean remove(Object o) {
+            boolean wasRemoved = set.remove(o);
+            if (wasRemoved) {
+                sem.release();
+            }
+            return wasRemoved;
+        }
+    }
+
+}
